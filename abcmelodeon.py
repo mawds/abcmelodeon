9f39ee2a01e0a1bf8dc3b69495ab05437023ce6c
David Mawdsley <david@mawds.co.uk>
2017-06-12 19:00:59 +0100

    Extract key from file

---
+++ abcmelodeon.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import re
+import argparse
+
+def getkey (infile):
+    """ Read an abc file and extract the key """
+    rxkey = re.compile(r'^K: ?(.+$)') 
+    key = None
+    with open(infile) as file:
+        for thisline in file:
+            m = rxkey.search(thisline)
+            if m:
+                if key is not None:
+                    raise ValueError("Multiple key lines found")
+                key = m.group(1)
+
+    return key
+
+
+def annotateabc (infile):
+    """ Annotate an abc file with button numbers """
+
+    key = getkey(infile)
+    print key
+
+
+parser = argparse.ArgumentParser(description = "Add button numbers to abc file")
+parser.add_argument("--infile", required=True)
+parser.add_argument("--outfile", required=True)
+
+args = parser.parse_args()
+
+annotatedabc = annotateabc(args.infile)
+
+
+
